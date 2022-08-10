ff5a214015f058ff6d5aad1f8062bc085dbe4c04
krishna kumar <krishnakumar@krishnas-MacBook-Pro.local>
2022-08-10 21:45:56 +0530

    argstest

--- validateDir.sh
+++ validateDir.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo "Hello World"
+echo $1
 echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
-echo "AGENT_WORKFOLDER contents:"
-ls -1 $AGENT_WORKFOLDER
+pwd
+ls
+if [ -d "HelloWorld/apiproxy" ] 
+then
+    echo "Directory /path/to/dir exists." 
+else
+    exit 9999
+fi
+echo "Hello one"
+echo "Hello two"
+echo "Hello three"
